7fba2c9718ea3dc57ec42beb5118ede0e61375fc
MKOUHOU <m.kouhou@bouygues-constrution.com>
2022-07-08 16:19:02 +0100

    first commit

---
+++ ManageFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string FileDirectory = @"C:\Users\DELL\source\repos\ManageFiles\ManageFiles\data\notes\";
+string ResumeDirectory = @"C:\Users\DELL\source\repos\ManageFiles\ManageFiles\data\resume\";
+string FileName = "note";
+string ResumeFileName = Path.Combine(ResumeDirectory,"resume-" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt");
+
+int count = 1;
+
+if (!Directory.Exists(ResumeDirectory)) Directory.CreateDirectory(ResumeDirectory);
+
+using (var writer = File.CreateText(ResumeFileName))
+{
+    string filename = FileDirectory + FileName + count + ".txt";
+    string data = string.Empty;
+
+    while (File.Exists(filename))
+    {   
+        
+        count++;
+        using (var reader = File.OpenText(filename))
+        {
+            data = reader.ReadToEnd();
+            await writer.WriteAsync(data+"\n");
+            filename = FileDirectory + FileName + count + ".txt";
+
+        }
+
+    }
+    
+}
